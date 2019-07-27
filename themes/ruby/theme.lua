c6b29565e17dc1f7f4775bb82af7325f07596bdf
M. Choji <mchoji@users.noreply.github.com>
2019-07-27 17:53:01 -0300

    Allow all kinds of icons

--- themes/ruby/theme.lua
+++ themes/ruby/theme.lua
@@ -31,8 +31,8 @@ theme:update()
 -- Desktop file parser
 --------------------------------------------------------------------------------
 theme.service.dfparser.icons.theme         = theme.homedir .. "/.icons/Flat-Remix-Green-Dark"
---theme.service.dfparser.icons.custom_only   = true 
---theme.service.dfparser.icons.scalable_only = true
+theme.service.dfparser.icons.custom_only   = false 
+theme.service.dfparser.icons.scalable_only = false
 
 
 -- Desktop config
